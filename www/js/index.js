f832bc88a2aad1dc1f2c9fecfb7f4db3bff4add3
AlbertoLosEnlaces <janunezlosenlaces@gmail.com>
2016-09-16 13:23:42 +0200

    app foto finished

--- www/js/index.js
+++ www/js/index.js
@@ -2,14 +2,28 @@ var app = {
     initialize: function() {
         console.log("inicializando app");
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        console.log("app inicializada");
+        console.log("app inicializada");        
+
     },
     
     onDeviceReady: function() {
         // app.receivedEvent('deviceready');
-        console.log("El dispositivo ya est´a preparado");
+        console.log("El dispositivo ya esta preparado");
+
+        //usa jquery
+        $("#btnfoto").click(function(event) {
+        	navigator.camera.getPicture(cameraSuccess, cameraError, {});
+        });
         
     }
 };
 
 app.initialize();
+
+function cameraSuccess () {
+	alert ("La camara ha funcionado bien");
+}
+
+function cameraError () {
+	alert ("La camara ha funcionado bien");
+}
